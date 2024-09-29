2aafe1ffafcda0e511c852f64c1dfa4d956a3f62
pcbproj <pcbproj@yandex.ru>
2024-09-29 21:19:58 +0300

    next commit

--- lib/inc/lcd1602.h
+++ lib/inc/lcd1602.h
@@ -3,6 +3,9 @@
 
 #include "stm32f407xx.h"
 
+//-------- LCD1602 pinout ---------------
+//WARNING: Function LCD1602_PinsInit() must be edited, if pinout was changed.
+
 #define RS_PORT		PORTB
 #define RW_PORT		PORTD
 #define E_PORT		PORTE
@@ -11,7 +14,6 @@
 #define DB5_PORT	PORTE
 #define DB4_PORT	PORTE
 
-
 #define RS_PIN_NUM	7
 #define RW_PIN_NUM	7
 #define E_PIN_NUM	1
@@ -19,6 +21,10 @@
 #define DB6_PIN_NUM	6
 #define DB5_PIN_NUM	5
 #define DB4_PIN_NUM	4
+//----------------------------------------
+
+#define CHAR_NUM_MAX	16
+#define STRING_NUM_MAX	2
 
 
 void LCD1602_PinsInit(void);
