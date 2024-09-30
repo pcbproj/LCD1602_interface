734c50da9638fbab9311fc02ef7eda331f4f08dc
pcbproj <pcbproj@yandex.ru>
2024-09-30 15:11:24 +0300

    lcd1602 in progress

---
+++ lib/inc/lcd1602_char_table.h
@@ -0,0 +1,8 @@
+#ifndef LCD1602_CHAR_TABLE_H
+#define LCD1602_CHAR_TABLE_H
+
+
+
+
+
+#endif
--- lib/src/lcd1602.c
+++ lib/src/lcd1602.c
@@ -1,1 +1,31 @@
 #include "lcd1602.h"
+
+
+
+void LCD1602_PinsInit4bits(void){
+	//-------- GPIO ports ckock enable --------
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
+
+	//------ pins output mode enable ------------
+	RS_PORT -> MODER |= (1 << RS_PIN_NUM*2);
+	RW_PORT -> MODER |= (1 << RW_PIN_NUM*2);
+	E_PORT -> MODER |= (1 << E_PIN_NUM*2);
+	DB7_PORT -> MODER |= (1 << DB7_PIN_NUM*2);
+	DB6_PORT -> MODER |= (1 << DB6_PIN_NUM*2);
+	DB5_PORT -> MODER |= (1 << DB5_PIN_NUM*2);
+	DB4_PORT -> MODER |= (1 << DB4_PIN_NUM*2);
+	
+	RS_CLR();
+	RW_CLR();
+}
+
+
+void LCD1602_ScreenInit4bits(void){
+	
+
+
+
+}
